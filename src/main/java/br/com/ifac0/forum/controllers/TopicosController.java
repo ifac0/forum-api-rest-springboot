b12ebc112e85f4da06d8604e241db825d10f4b63
Ivan Costa <ivan.costa@justa.com.vc>
2020-05-30 16:32:42 -0300

    fix(controller): :bug: Correção na anotação colocada no controller no metodo post;
    Signed-off-by: Ivan Costa <ivan.costa@justa.com.vc>

--- src/main/java/br/com/ifac0/forum/controllers/TopicosController.java
+++ src/main/java/br/com/ifac0/forum/controllers/TopicosController.java
@@ -1,14 +1,17 @@
 package br.com.ifac0.forum.controllers;
 
+import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.ifac0.forum.controllers.dto.TopicoDto;
 import br.com.ifac0.forum.controllers.form.TopicoForm;
@@ -38,9 +41,11 @@ public class TopicosController {
 	}
 	
 	@PostMapping
-	@RequestMapping("/topicos")
-	public void cadastrar(@RequestBody TopicoForm form) {
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
+		
+		URI uri =  uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		return ResponseEntity.created(uri).body(new TopicoDto(topico));
 	}
 }
