1e641d2f6b22fad350ce33076f4a9d13cfe89975
Ivan Costa <ivan.costa@justa.com.vc>
2020-05-30 17:20:41 -0300

    feat(Validation): Adiciona validação no from de Topico
    Signed-off-by: Ivan Costa <ivan.costa@justa.com.vc>

--- src/main/java/br/com/ifac0/forum/controllers/form/TopicoForm.java
+++ src/main/java/br/com/ifac0/forum/controllers/form/TopicoForm.java
@@ -1,13 +1,28 @@
 package br.com.ifac0.forum.controllers.form;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
 import br.com.ifac0.forum.models.Curso;
 import br.com.ifac0.forum.models.Topico;
 import br.com.ifac0.forum.repositorys.CursoRepository;
 
 public class TopicoForm {
 	
+	@NotNull
+	@NotEmpty	
+	@Length(min = 5)
 	private String titulo;
+	
+	@NotNull
+	@NotEmpty
+	@Length(min = 10)
 	private String mensagem;
+	
+	@NotNull
+	@NotEmpty	
 	private String nomeCurso;
 	
 	public String getTitulo() {
