efded2428b7397a5dbbe38363d7d89c2f6f13add
Ivan Costa <ivan.costa@justa.com.vc>
2020-05-31 23:18:23 -0300

    feat(AtualizarTopico): Adiciona metodo no controller para atualizar o topico; Adiciona um form para receber os dados que vao ser atualizados;
    Signed-off-by: Ivan Costa <ivan.costa@justa.com.vc>

--- src/main/java/br/com/ifac0/forum/controllers/TopicosController.java
+++ src/main/java/br/com/ifac0/forum/controllers/TopicosController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +20,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.ifac0.forum.controllers.dto.DetalhesTopicoDto;
 import br.com.ifac0.forum.controllers.dto.TopicoDto;
+import br.com.ifac0.forum.controllers.form.AtualizacaoTopicoForm;
 import br.com.ifac0.forum.controllers.form.TopicoForm;
 import br.com.ifac0.forum.models.Curso;
 import br.com.ifac0.forum.models.Topico;
@@ -61,7 +64,13 @@ public class TopicosController {
 	@GetMapping("/{id}")
 	public DetalhesTopicoDto detalhar(@PathVariable Long id) {
 		Topico topico = topicoRepository.getOne(id);
-		return new DetalhesTopicoDto(topico);
-		
+		return new DetalhesTopicoDto(topico);		
+	}
+	
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form){
+		Topico topico = form.atualizar(id, topicoRepository);
+		return ResponseEntity.ok(new TopicoDto(topico));
 	}
 }
