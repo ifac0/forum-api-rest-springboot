5c4a19b392ec606335a088c9569c0fe4f03d3ce5
Ivan Costa <ivan.costa@justa.com.vc>
2020-05-31 23:22:57 -0300

    feat(DeleteTopico): Adiciona a funcionalidade de deletar um topico;
    Signed-off-by: Ivan Costa <ivan.costa@justa.com.vc>

--- src/main/java/br/com/ifac0/forum/controllers/TopicosController.java
+++ src/main/java/br/com/ifac0/forum/controllers/TopicosController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,6 +49,7 @@ public class TopicosController {
 	}
 	
 	@PostMapping
+	@Transactional
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
@@ -72,5 +74,12 @@ public class TopicosController {
 	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form){
 		Topico topico = form.atualizar(id, topicoRepository);
 		return ResponseEntity.ok(new TopicoDto(topico));
+	}
+	
+	@DeleteMapping("{id}")
+	@Transactional
+	public ResponseEntity<TopicoDto> remover(@PathVariable Long id){
+		topicoRepository.deleteById(id);
+		return ResponseEntity.ok().build();
 	}
 }
